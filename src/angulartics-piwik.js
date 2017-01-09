551e92c4769797fda67bdfc2ee85abcad0e2962a
Rastislav Bertusek <rastislav.bertusek@gmail.com>
2017-01-09 09:40:51 +0100

    Update angulartics-piwik.js

--- src/angulartics-piwik.js
+++ src/angulartics-piwik.js
@@ -137,6 +137,22 @@
                         $window._paq.push(['trackEvent', properties.category, action, properties.label, properties.value]);
                     }
                 });
+                
+              /**
+               * Exception Track Event in GA
+               * @name exceptionTrack
+               * Sugar on top of the eventTrack method for easily handling errors
+               *
+               * @param {object} error An Error object to track: error.toString() used for event 'action', error.stack used for event 'label'.
+               * @param {object} cause The cause of the error given from $exceptionHandler, not used.
+               *
+               * @link https://developers.google.com/analytics/devguides/collection/analyticsjs/events
+               */
+              $analyticsProvider.registerExceptionTrack(function (error, cause) {
+                  if ($window._paq) {
+                      $window._paq.push(['trackEvent', 'Exceptions', error.toString(), error.stack, 0]);
+                  }
+              });
             }
         ]);
 })(angular);
